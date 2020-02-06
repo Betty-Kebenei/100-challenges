726c3a5a1b7b937c8ffba7a76ebca6b3c17a4b3c
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-02-06 16:12:15 +0300

    fix bubble sort

--- data-structures-and-algorithms/bubbleSort.js
+++ data-structures-and-algorithms/bubbleSort.js
@@ -1,17 +1,38 @@
-function bubbleSort(arr) {
+// solution 1
+function bubbleSort1(arr) {
     for(let i=0; i<arr.length; i++){
-        for(let j=i+1; j<arr.length; j++){
-            if(arr[i] > arr[j]){
-                const k = arr[i];
-                arr[i] = arr[j];
-                arr[j] = k
+        for(let j=i; j<arr.length; j++){
+            if(arr[j] > arr[j + 1]){
+                const k = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = k
             }
         }
     }
     return arr;
 }
 
+// Solution 1 runs on already sorted arrary more than once
+// solution 2
+
+function bubbleSort2(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
+}
+
+
 const arr = [20, 1, 4, 3, 12, 9, 10, 40];
 console.log(arr);
-bubbleSort(arr);
+bubbleSort2(arr);
 console.log(arr);
