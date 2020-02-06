59098483a057f1cf4a2a5b32e70a227e8925f98d
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-02-06 16:33:07 +0300

    add insertion sort c5

--- data-structures-and-algorithms/insertionSort.js
+++ data-structures-and-algorithms/insertionSort.js
@@ -1,7 +1,7 @@
 function inserionSort(arr) {
     for(let i=0; i<arr.length; i++){
-        for(let j=i+1; j<arr.length; j++){
-            if(arr[i] > arr[j]){
+        for(let j=0; j<i; j++){
+            if(arr[i] < arr[j]){
                 const k = arr[i];
                 arr[i] = arr[j];
                 arr[j] = k
@@ -11,7 +11,7 @@ function inserionSort(arr) {
     return arr;
 }
 
-const arr = [20, 1, 4, 3, 12, 9, 10, 40];
+const arr = [20, 4, 1, 3, 12, 9, 10, 40];
 console.log(arr);
 inserionSort(arr);
 console.log(arr);
